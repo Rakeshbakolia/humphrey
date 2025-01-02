2f41110a8b6b6a8457570bcf6a186c615554a76a
Rakesh Bakolia <rakeshbakolia@Rakeshs-MacBook-Air.local>
2025-01-03 03:28:18 +0530

    store in s3

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,26 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // ...
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            credentials: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+            },
+            region: env('AWS_REGION'),
+            params: {
+              Bucket: env('AWS_BUCKET'),
+              ACL: null
+            },
+          },
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
